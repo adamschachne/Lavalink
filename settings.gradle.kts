00bb2552d47ef6e0ca0f09b125d9a4cada1f4853
topi314 <git@topi.wtf>
2026-03-01 00:38:39 +0100

    update libdave version to 4afcde442

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -44,7 +44,7 @@ fun VersionCatalogBuilder.voice() {
     library("koe",          "moe.kyokobot.koe", "core").versionRef("koe")
     library("koe-udpqueue", "moe.kyokobot.koe", "ext-udpqueue").versionRef("koe")
 
-    version("libdave", "aa0e7f78e")
+    version("libdave", "4afcde442")
     val libDavePlatforms = listOf("linux-x86-64", "linux-x86", "linux-aarch64", "linux-arm", "linux-musl-x86-64", "linux-musl-aarch64", "win-x86-64", "win-x86", "darwin")
     libDavePlatforms.forEach {
         library("libdave-natives-$it", "moe.kyokobot.libdave", "natives-$it").versionRef("libdave")
