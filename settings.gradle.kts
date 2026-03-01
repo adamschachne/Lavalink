d1bfb8c3a5c6ceffa21d75876c1417b223b07fc9
topi314 <git@topi.wtf>
2026-03-01 22:19:43 +0100

    update koe to version 3.0.0-pre5 and update libdave to version 0.1.0

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -35,7 +35,7 @@ fun VersionCatalogBuilder.spring() {
 
 fun VersionCatalogBuilder.voice() {
     version("lavaplayer", "1.5.6")
-    version("koe", "3.0.0-pre3")
+    version("koe", "3.0.0-pre5")
 
     library("lavaplayer",            "dev.arbjerg", "lavaplayer").versionRef("lavaplayer")
     library("lavaplayer-ip-rotator", "dev.arbjerg", "lavaplayer-ext-youtube-rotator").versionRef("lavaplayer")
@@ -44,7 +44,7 @@ fun VersionCatalogBuilder.voice() {
     library("koe",          "moe.kyokobot.koe", "core").versionRef("koe")
     library("koe-udpqueue", "moe.kyokobot.koe", "ext-udpqueue").versionRef("koe")
 
-    version("libdave", "4afcde442")
+    version("libdave", "0.1.0")
     val libDavePlatforms = listOf("linux-x86-64", "linux-x86", "linux-aarch64", "linux-arm", "linux-musl-x86-64", "linux-musl-aarch64", "win-x86-64", "win-x86", "darwin")
     libDavePlatforms.forEach {
         library("libdave-natives-$it", "moe.kyokobot.libdave", "natives-$it").versionRef("libdave")
