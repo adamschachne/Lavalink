c5cb5a0d85d699b039f655614af3e2de0a5d8c61
Jakub Kuczys <me@jacken.men>
2026-03-06 02:00:01 +0100

    Revert "Use tagged version"
    This reverts commit 0c8c5870b27d9a22740f8fbd1093caf45c9bfc91.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -44,7 +44,7 @@ fun VersionCatalogBuilder.voice() {
     library("koe",          "moe.kyokobot.koe", "core").versionRef("koe")
     library("koe-udpqueue", "moe.kyokobot.koe", "ext-udpqueue").versionRef("koe")
 
-    version("libdave", "0.1.2")
+    version("libdave", "ce725965e")
     val libDavePlatforms = listOf("linux-x86-64", "linux-x86", "linux-aarch64", "linux-arm", "linux-musl-x86-64", "linux-musl-aarch64", "win-x86-64", "win-x86", "darwin")
     libDavePlatforms.forEach {
         library("libdave-natives-$it", "moe.kyokobot.libdave", "natives-$it").versionRef("libdave")
